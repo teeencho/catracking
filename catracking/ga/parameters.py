02e8b0de646a45f082f1b3eba8cbc9c3f2472550
Ramon Saraiva <ramonsaraiva@gmail.com>
2017-10-31 11:58:23 -0200

    Added initial structure and constants

---
+++ catracking/ga/parameters.py
@@ -0,0 +1,22 @@
+"""
+These parameters are part of the Measurement Protocol definition.
+More information can be found at:
+developers.google.com/analytics/devguides/collection/protocol/v1/parameters
+"""
+
+VERSION = 'v'
+HIT_TYPE = 't'
+TRACKING_ID = 'tid'
+USER_ID = 'uid'
+USER_IP = 'uip'
+CLIENT_ID = 'cid'
+USER_AGENT = 'ua'
+DATASOURCE = 'ds'
+EVENT_CATEGORY = 'ec'
+EVENT_ACTION = 'ea'
+EVENT_LABEL = 'el'
+EVENT_VALUE = 'ev'
+EVENT_NON_INTERACTIVE = 'ni'
+HIT_TYPE_EVENT = 'event'
+DOCUMENT_HOSTNAME = 'dh'
+CACHE_BUSTER = 'z'
