a09b8f54b65b2ebeda112ffa4c03fdb84e197a60
Ramon Saraiva <ramonsaraiva@gmail.com>
2017-11-02 19:05:26 -0200

    Added different types of chunks and their behaviour. This makes the API easier to be handled in a view

--- catracking/ga/core.py
+++ catracking/ga/core.py
@@ -23,8 +23,8 @@ class GoogleAnalyticsTracker(Tracker):
     def __init__(self, request):
         super(GoogleAnalyticsTracker, self).__init__()
         self.request = request
-        self.pool = []
-        self._base_hit = None
+        self.hits = []
+        self._root_chunk = None
 
     @staticmethod
     def generate_ga_client_id():
@@ -36,23 +36,68 @@ class GoogleAnalyticsTracker(Tracker):
         timestamp = str(int(time()))
         return 'GA1.1.{0}'.format('.'.join([random_string, timestamp]))
 
-    def get_base_hit(self):
-        if not self._base_hit:
-            self._base_hit = MeasurementProtocolHit(self.request)
-        return self._base_hit
+    def get_root_chunk(self):
+        if not self._root_chunk:
+            self._root_chunk = RootHitChunk(self.request)
+        return self._root_chunk
+
+    def new_event(self, category, action, label, value=0, non_interactive=1):
+        event = EventHitChunk(category, action, label, value, non_interactive)
+        self.hits.append(event)
+        return event
+
+    def new_pageview(self):
+        raise NotImplementedError('Pageview event is not implemented yet')
+
+    def compile(self):
+        pass
 
     def send(self):
         pass
 
 
-class MeasurementProtocolHit(OrderedDict):
+class BaseMeasurementProtocolHit(OrderedDict):
+    """
+    Additional functionality for the OrderedDict, as a Base for any hit chunk.
+    """
+
+    def __init__(self):
+        super(BaseMeasurementProtocolHit, self).__init__()
+
+    def __add__(self, partial):
+        self.update(partial)
+        return self
+
+    def __iadd__(self, partial):
+        self.update(partial)
+        return self
+
+    def __setitem__(self, key, value):
+        """
+        If the value being added to the dictionary is either `None` or an
+        empty string, we should not have its key.
+        """
+        if not (value is None or value is ''):
+            super(BaseMeasurementProtocolHit, self).__setitem__(key, value)
+
+    @property
+    def encoded_url(self):
+        """
+        Returns the full encoded url for the hit.
+        Cache buster parameter should be the last in the querystring.
+        """
+        self[parameters.CACHE_BUSTER] = random.randint(1, 100000)
+        return urlencode(self)
+
+
+class RootHitChunk(BaseMeasurementProtocolHit):
     """
     All information added to the base hit is required and should be the same
     for every type of hit.
     """
 
     def __init__(self, request):
-        super(MeasurementProtocolHit, self).__init__()
+        super(RootHitChunk, self).__init__()
         self.request = request
         self[parameters.VERSION] = 1
         self[parameters.TRACKING_ID] = self.ga_property
@@ -114,17 +159,57 @@ class MeasurementProtocolHit(OrderedDict):
         except AttributeError:
             return 0
 
-    @property
-    def encoded_url(self):
+
+class HitProductsMixin(object):
+    """
+    Used whenever a hit chunk is able to contain products.
+    For example, a custom event or a pageview.
+    """
+
+    def __init__(self):
+        super(HitProductsMixin, self).__init__()
+        self._transaction = None
+        self._products = []
+
+    def new_transaction(self, id, affiliation=None, revenue=None):
         """
-        Returns the full encoded url for the hit.
-        Cache buster parameter should be the last in the querystring.
+        Creates a transaction chunk and returns it, in case extra (not default)
+        dimensions or metrics need to be added.
         """
-        self[parameters.CACHE_BUSTER] = random.randint(1, 100000)
-        return urlencode(self)
+        self._transaction = TransactionHitChunk(id, affiliation, revenue)
+        return self._transaction
+
+    def set_product_action(self, action):
+        self[parameters.PRODUCT_ACTION] = action
+
+    def new_product(self, id=None, name=None, category=None, brand=None,
+                    price=None, quantity=None):
+        """
+        Creates a product chunk and returns it, in case extra (not default)
+        dimensions or metrics need to be added.
+        """
+        index = len(self._products) + 1
+        product = ProductHitChunk(
+            index, id, name, category, brand, price, quantity)
+        self._products.append(product)
+        return product
+
+    def compile(self):
+        """
+        Merges the transaction and products into the hit, new transaction or
+        new products should not be added after this.
+        """
+        if self._transaction:
+            self += self._transaction
+        for product in self._products:
+            self += product
 
 
-class EventHit(OrderedDict):
+class PageViewHitChunk(HitProductsMixin, BaseMeasurementProtocolHit):
+    pass
+
+
+class EventHitChunk(HitProductsMixin, BaseMeasurementProtocolHit):
     """
     Builds part of a hit, this should be merged to the base of the
     Measurement Protocol hit.
@@ -133,15 +218,50 @@ class EventHit(OrderedDict):
     additional data can be appended to the object (custom dimensions, metrics)
     """
 
-    def __init__(self, category, action, label, value, non_interactive=0):
+    def __init__(self, category, action, label, value=0, non_interactive=0):
         """
         Events are non interactive by default, using `non_interactive=0`
         makes it interactive and will affect the bounce rate data
         """
-        super(EventHit, self).__init__()
+        super(EventHitChunk, self).__init__()
         self[parameters.HIT_TYPE] = parameters.HIT_TYPE_EVENT
         self[parameters.EVENT_CATEGORY] = category
         self[parameters.EVENT_ACTION] = action
         self[parameters.EVENT_LABEL] = label
         self[parameters.EVENT_VALUE] = value
         self[parameters.EVENT_NON_INTERACTIVE] = non_interactive
+
+
+class TransactionHitChunk(BaseMeasurementProtocolHit):
+    """
+    Builds part of a hit, this should be merged to the base of the
+    Measurement Protocol hit.
+
+    Adds the information required for the `transaction` chunk in a hit.
+    """
+
+    def __init__(self, id, affiliation=None, revenue=None):
+        super(TransactionHitChunk).__init__()
+        self[parameters.TRANSACTION_ID] = id
+        if affiliation:
+            self[parameters.TRANSACTION_AFFILIATION] = affiliation
+        if revenue:
+            self[parameters.TRANSACTION_REVENUE] = revenue
+
+
+class ProductHitChunk(BaseMeasurementProtocolHit):
+
+    def __init__(self, index, id=None, name=None, category=None, brand=None,
+                 price=None, quantity=None):
+        super(ProductHitChunk, self).__init__()
+        self.index = index
+        self[parameters.PRODUCT_ID] = id
+        self[parameters.PRODUCT_NAME] = name
+        self[parameters.PRODUCT_CATEGORY] = category
+        self[parameters.PRODUCT_BRAND] = brand
+        self[parameters.PRODUCT_PRICE] = price
+        self[parameters.PRODUCT_QUANTITY] = quantity
+
+    def __setitem__(self, key, value):
+        key = 'pr{0}{1}'.format(self.index, key)
+        super(ProductHitChunk, self).__setitem__(key, value)
