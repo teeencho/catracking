cf67ff73fcad54b1ead156172976177fa6a32200
Nathan Rowlan <nrowlan@consumeraffairs.com>
2017-11-09 13:25:21 -0600

    SYS-252 account for missing TTY

--- distelli/prerelease.sh
+++ distelli/prerelease.sh
@@ -3,7 +3,7 @@ set -ex
 
 test_container_name=catracking:br-${DISTELLI_RELBRANCH}-${DISTELLI_RELREVISION:0:7}-${DISTELLI_BUILDNUM}
 
-docker run --rm -it \
+docker run --rm \
     -e CODECOV_TOKEN=$CODECOV_TOKEN \
     $test_container_name \
     tox
