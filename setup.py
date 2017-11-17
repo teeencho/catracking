d82fdeea6fa450ce2cf37435fba90f6dfd5165fc
Ramon Saraiva <ramonsaraiva@gmail.com>
2017-11-17 13:28:18 -0200

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import (
 
 setup(
     name='catracking',
-    version='0.1.0',
+    version='0.1.2',
     author='ConsumerAffairs',
     description='Tracking for ConsumerAffairs',
     url='https://github.com/ConsumerAffairs/catracking',
