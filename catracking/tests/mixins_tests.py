b664434e80175cd51d0c1bf0e3ee16dee338b165
Ramon Saraiva <ramonsaraiva@gmail.com>
2017-12-06 12:30:41 -0200

    Add call method to mixin, following same django implementation on 1.10

--- catracking/tests/mixins_tests.py
+++ catracking/tests/mixins_tests.py
@@ -9,12 +9,58 @@ class MiddlewareMixinTest(TestCase):
 
     def setUp(self):
         self.middleware_class = MiddlewareMixin
+        self.middleware = self.middleware_class()
 
     def test_init_no_get_response(self):
-        middleware = self.middleware_class()
-        self.assertIsNone(middleware.get_response)
+        self.assertIsNone(self.middleware.get_response)
 
     def test_init_get_response(self):
         get_response = mock.MagicMock()
-        middleware = self.middleware_class(get_response)
-        self.assertEquals(middleware.get_response, get_response)
+        self.middleware = self.middleware_class(get_response)
+        self.assertEquals(self.middleware.get_response, get_response)
+
+    def test_call_with_process_request(self):
+        process_request = mock.MagicMock()
+        request = mock.MagicMock()
+        expected_response = mock.MagicMock()
+
+        setattr(self.middleware, 'process_request', process_request)
+        process_request.return_value = expected_response
+        response = self.middleware(request)
+        self.assertEquals(expected_response, response)
+
+    def test_call_without_process_request(self):
+        get_response = mock.MagicMock()
+        request = mock.MagicMock()
+        expected_response = mock.MagicMock()
+        get_response.return_value = expected_response
+
+        self.middleware = self.middleware_class(get_response)
+        response = self.middleware(request)
+        self.assertEquals(expected_response, response)
+
+    def test_call_with_process_response(self):
+        request = mock.MagicMock()
+        expected_response = mock.MagicMock()
+        get_response = mock.MagicMock()
+        get_response.return_value = expected_response
+        process_response = mock.MagicMock()
+        process_response.return_value = expected_response
+        setattr(self.middleware, 'process_response', process_response)
+
+        self.middleware = self.middleware_class(get_response)
+        response = self.middleware(request)
+        self.assertEquals(expected_response, response)
+
+    def test_call_with_process_request_and_process_response(self):
+        request = mock.MagicMock()
+        expected_response = mock.MagicMock()
+        process_request = mock.MagicMock()
+        process_request.return_value = expected_response
+        process_response = mock.MagicMock()
+        process_response.return_value = expected_response
+        setattr(self.middleware, 'process_request', process_request)
+        setattr(self.middleware, 'process_response', process_response)
+
+        response = self.middleware(request)
+        self.assertEquals(expected_response, response)
